86ec72b862864e3fe7d33bb6d08a3b4da99df730
Alexander Surma <alexander.surma@gmail.com>
2010-09-24 12:53:01 +0200

    Improved Errorhandling

--- backup.go
+++ backup.go
@@ -18,9 +18,19 @@ var (
 	AUTHORS = "Alexander \"Surma\" Surma <surma@78762.de>"
 )
 
+func HandleError(prefix string, e Error) {
+	if e.Occurred() {
+		ShowHelp(true)
+		panic(prefix+": "+e.String()+"\n")
+	}
+}
+
 func main() {
 	conf := new(BackupConf)
 	_, e := SetupEnv(conf)
+	
+	HandleError("SetupEnv", e)
+	w.DumpWarnings(os.Stderr)
 
 	if e != nil {
 		panic("Backup: "+e.String())
